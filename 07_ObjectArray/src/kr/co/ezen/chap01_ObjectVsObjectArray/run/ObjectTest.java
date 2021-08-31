55688d38385cb078f43283dcffc6cae53c61cf67
Haram Jeong <niceharam@gmail.com>
2021-08-31 20:37:59 +0900

    20일차 - 210831

---
+++ 07_ObjectArray/src/kr/co/ezen/chap01_ObjectVsObjectArray/run/ObjectTest.java
@@ -0,0 +1,32 @@
+package kr.co.ezen.chap01_ObjectVsObjectArray.run;
+
+import java.util.Scanner;
+
+import kr.co.ezen.chap01_ObjectVsObjectArray.model.vo.Book;
+
+public class ObjectTest {
+
+	public static void main(String[] args) {
+		Book bk1 = new Book("자바 기초1", "홍길동1", "EZEN1", 10000);
+		Book bk2 = new Book("자바 기초2", "홍길동2", "EZEN2", 20000);
+		Book bk3 = new Book("자바 기초3", "홍길동3", "EZEN3", 30000);
+
+		System.out.println(bk1.information());
+		System.out.println(bk2.information());
+		System.out.println(bk3.information());
+
+		// 도서 검색
+		Scanner sc = new Scanner(System.in);
+		System.out.println("검색할 책 제목: ");
+		String searchTitle = sc.nextLine();
+
+		if (bk1.getTitle().equals(searchTitle)) {
+			System.out.println(bk1.information());
+		} else if (bk2.getTitle().equals(searchTitle)) {
+			System.out.println(bk2.information());
+		} else if (bk3.getTitle().equals(searchTitle)) {
+			System.out.println(bk3.information());
+		}
+	}
+
+}
